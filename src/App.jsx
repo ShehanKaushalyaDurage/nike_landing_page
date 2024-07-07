a124c7201c6a285598c3321e97373ea4d78a4219
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-07-08 00:08:51 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const APP = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      Populer product
+    </section>
+    <section className="padding">
+      Super Quality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      Speacial Offers
+    </section>
+    <section className="padding bg-pale-blue">
+      Customer Reviews
+    </section>
+    <section className="padding-x sm:py-32 w-full">
+      Subscribe
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      Footer
+    </section>
+  </main>
+);
+
+export default APP;
