ab3ea0030e6b3389dccc7eadbeadb4c064814c1a
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-07-12 01:12:10 +0530

    done

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,3 +1,4 @@
+import { arrowRight } from "../assets/icons";
 import { shoe8 } from "../assets/images";
 import Button from "../components/Button";
 
@@ -23,10 +24,10 @@ const SuperQuality = () => {
             Our dedication to detail and excellence ensures your satisfaction
           </p>
           <div className="mt-11">
-            <Button label="View Details" />
+            <Button label="Shop Now" iconURL={arrowRight}/>
           </div>
         </div>
-        <div className="flex-1 justify-center items-center ">
+        <div className="flex-1 flex justify-center items-center ">
             <img src={shoe8} alt="shoe 8" width={570} height={522} className="object-contain"/>
         </div>
       </section>
