47d69d65a6a931d0d09f39f0874291c5223608a8
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-07-12 00:10:51 +0530

    dolorum

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { star } from "../assets/icons"
 
-const PopularProductCard = () => {
+const PopularProductCard = ({ imgURL,name,price}) => {
   return (
-    <div>
-      
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+      <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+      <div className="mt-8 flex justify-start gap-2.5 ">
+        <img src={star} alt="raiting" width={24} height={24} />
+        <p className="font-montserrat text-xl leading-normal text-slate-gray">4.5</p>
+      </div>
     </div>
   )
 }
