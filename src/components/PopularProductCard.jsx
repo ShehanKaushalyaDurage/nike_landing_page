57aa66ed5a2011c3b1dabe8b396a31a110ebaed2
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-07-12 00:14:43 +0530

    popular product section done

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -8,6 +8,8 @@ const PopularProductCard = ({ imgURL,name,price}) => {
         <img src={star} alt="raiting" width={24} height={24} />
         <p className="font-montserrat text-xl leading-normal text-slate-gray">4.5</p>
       </div>
+      <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">{name}</h3>
+      <p className="mt-2 font-semibold text-coral-red font-montserrat text-2lx leading-normal">{price}</p>
     </div>
   )
 }
