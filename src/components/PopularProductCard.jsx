db35796e6a414e9e03b036b271d4a540af85984a
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-07-12 00:15:28 +0530

    dolorum

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -6,7 +6,7 @@ const PopularProductCard = ({ imgURL,name,price}) => {
       <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
       <div className="mt-8 flex justify-start gap-2.5 ">
         <img src={star} alt="raiting" width={24} height={24} />
-        <p className="font-montserrat text-xl leading-normal text-slate-gray">4.5</p>
+        <p className="font-montserrat text-xl leading-normal text-slate-gray">( 4.5 )</p>
       </div>
       <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">{name}</h3>
       <p className="mt-2 font-semibold text-coral-red font-montserrat text-2lx leading-normal">{price}</p>
